f7028e58961d6ce46edac77f84f8f18a4fdeda28
Wallop Opasakhun <champbassist159@gmail.com>
2019-10-15 18:13:11 +0700

    add mock statement for checking email and password

--- src/redux/auth/reducer.js
+++ src/redux/auth/reducer.js
@@ -4,10 +4,19 @@ const INIT_STATE = {
     user: localStorage.getItem('user')
 };
 
+const mockUser = {
+    email: "ben@ezygroup.co",
+    password: "1234"
+}
+
 export default (state = INIT_STATE, action) => {
     switch (action.type) {
         case LOGIN:
-            localStorage.setItem('user', action.payload.email);
+            const user = action.payload;
+            if (user.email != mockUser.email || user.password != mockUser.password) {
+                return { ...state, message: 'invalid email or password' };
+            }
+            localStorage.setItem('user', user.email);
             return {
                 ...state,
                 user: action.payload
