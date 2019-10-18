d077fac75e21629996c79372e35a01d6baddfd49
Wallop Opasakhun <champbassist159@gmail.com>
2019-10-18 17:41:18 +0700

    add main page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,25 @@
 import React, { Component, Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Guest from './guest';
+import Main from './main';
 
-class MainApp extends Component {
+class App extends Component {
     render() {
         const { match } = this.props;
         return (
             <Fragment>
                 <Switch>
                     {/* Register your global routes here. */}
-                    <Route path={`${match.url}`} component={Guest} />
+                    <Route path={`${match.url}`} component={user ? Main : Guest} />
                 </Switch>
             </Fragment>
         );
     }
 }
 
-export default MainApp;
+const mapStateToProps = ({ auth }) => ({
+    user: auth.user
+});
+
+export default connect(mapStateToProps)(App);
