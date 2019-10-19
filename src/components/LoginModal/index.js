aeb3faaf4238606960787199fa180edc5775753d
yeentrp <alongkoninsuwan@gmail.com>
2019-10-19 11:46:31 +0700

    log in modal

--- src/components/LoginModal/index.js
+++ src/components/LoginModal/index.js
@@ -1,21 +1,97 @@
 import React, { Component, Fragment } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+import {Modal, ModalHeader, ModalBody, Row,Form,Label,Input,Button,Container} from 'reactstrap'
 import { toggleModal, openModal } from 'Redux/actions';
 import { connect } from 'react-redux';
+
+const initialState = {
+    email: "",
+    password: "",
+    emailError: "",
+    passwordError: ""
+  };
+
 class LoginModal extends Component {
+  
+    
+    state = initialState;
+  
+    handleChange = event => {
+        const isCheckbox = event.target.type === "checkbox";
+        this.setState({
+          [event.target.name]: isCheckbox
+            ? event.target.checked
+            : event.target.value
+        });
+      };
+    
+      validate = () => {
+        let emailError = "";
+        // let passwordError = "";
+    
+        
+    
+        if (!this.state.email.includes("@")) {
+          emailError = "invalid email";
+        }
+        if (emailError ) {
+          this.setState({ emailError,  });
+          return false;
+        }
+    
+        return true;
+      };
+    
+      handleSubmit = event => {
+        event.preventDefault();
+        const isValid = this.validate();
+        if (isValid) {
+          console.log(this.state);
+          // clear form
+          this.setState(initialState);
+        }
+      };
     render() {
         const { isOpen, toggleModal, openModal } = this.props;
         return (
             <Fragment>
                 <Modal isOpen={isOpen} toggle={() => toggleModal('loginModal')} backdrop={true} className='loginModal'>
-                    <ModalHeader>Modal title</ModalHeader>
+                    <div class="d-flex justify-content-center">
+                        <ModalHeader>
+                            ACCOUNT LOGIN
+                        </ModalHeader>
+                    </div>
                     <ModalBody>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        <Form onSubmit={this.handleSubmit}>
+                            <div class="form-group">
+                            <br></br>
+                                <Label for="exampleInputEmail1">Email address</Label>
+                                <Input type="email" 
+                                name="email"
+                                    class="form-control"
+                                    placeholder="example@indezy.com"
+                                    value={this.state.email}
+                                    onChange={this.handleChange}>
+                                </Input>
+                                <div style={{ fontSize: 12, color: "red" }}>
+                                {this.state.emailError}
+                                </div>
+                                <Label for="exampleInputPassword1">Password</Label>
+                                <Input 
+                                name="password"
+                                type="password"
+                                    class="form-control"
+                                    placeholder="Password"
+                                    value={this.state.password}
+                                    onChange={this.handleChange}
+                                    >
+                                </Input>
+                                <div style={{ fontSize: 12, color: "red" }}>
+            {this.state.passwordError}</div>
+                                <br></br>
+                                <Button type = "submit"color="primary" size="lg" active>LOG IN</Button>
+                            </div>
+                        </Form>
                     </ModalBody>
-                    {/* <ModalFooter>
-                        <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
-                        <Button color="secondary">Cancel</Button>
-                    </ModalFooter> */}
                 </Modal>
             </Fragment>
         );
@@ -26,4 +102,4 @@ const mapStateToProps = ({ modals }) => (
     { isOpen: modals.loginModal }
 );
 
-export default connect(mapStateToProps, { toggleModal , openModal })(LoginModal);
+export default connect(mapStateToProps, { toggleModal, openModal })(LoginModal);
