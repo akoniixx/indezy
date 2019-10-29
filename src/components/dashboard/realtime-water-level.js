da0930275a26959bb447264fb5a55b587286193a
Wallop Opasakhun <champbassist159@gmail.com>
2019-10-30 01:41:48 +0700

    propped critical_min and critical_max

--- src/components/dashboard/realtime-water-level.js
+++ src/components/dashboard/realtime-water-level.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Prototype from './prototype';
 import styled from 'styled-components';
 import { isCritical } from 'Utils';
+import { connect } from 'react-redux';
 
-export default (props) => {
+const RealtimeWaterLevel = (props) => {
     props = {
         ...props,
         unit: 'Meters',
@@ -11,9 +12,10 @@ export default (props) => {
     }
     //! Critical zone calculation
     const { value } = props;
-    const critical_min = 50;
-    const critical_max = 60;
+    const critical_min = props.critical_min || null;
+    const critical_max = props.critical_max || null;
     const isInCriticalZone = !isCritical(value, critical_min, critical_max);
+    console.log('Realtime Water Level', `Value is ${isInCriticalZone ? '' : 'not'} in critical zone`, critical_min, critical_max);
     //! Critical zone calculation end
     const height = 156;
     const levelPercent = 56;
@@ -26,6 +28,13 @@ export default (props) => {
         </Prototype>
     );
 }
+
+const mapStateToProps = () => ({
+    critical_min: 50,
+    critical_max: 60
+})
+
+export default connect(mapStateToProps)(RealtimeWaterLevel);
 
 const DistanceWrapper = styled.div`
 width: 170px;
